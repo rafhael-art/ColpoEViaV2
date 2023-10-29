e8a9b000df0651554527e49d28dd1c4a94479c34
Rafhael <rafhaelpillaca2001@gmail.com>
2023-10-29 00:54:42 -0500

    Initial Commit

---
+++ MessengerClone/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MessengerClone
+//
+//  Created by Rafhael Pillaca Pariona on 29/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MessengerClone/MessengerCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  MessengerCloneApp.swift
+//  MessengerClone
+//
+//  Created by Rafhael Pillaca Pariona on 29/10/23.
+//
+
+import SwiftUI
+
+@main
+struct MessengerCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
